3888a56ae1f5ddbfe9a77745ca1958ef7859b2e4
John-1005 <jtgarza48@gmail.com>
2025-05-05 15:47:23 -0700

    Fixed for loops in test

--- repl_test.go
+++ repl_test.go
@@ -6,23 +6,28 @@ import "testing"
 
 func TestCleanInput(t *Testing.T) {
   cases := []struct {
-    input string
-    expected []string
-  }{
-
-    input: "  hello world ",
-    expected: []string {"hello", "world"},
-  },
-  //Add more test cases just setting up for now
+	input    string
+	expected []string
+}{
+	{
+		input:    "  hello  world  ",
+		expected: []string{"hello", "world"},
+	},
+	// add more cases here
+}
 
   for _, c := range cases {
-    actual := cleanInput(c.input)
-    //Need to check the length of the actual silce against the expected silce
-    //if they don't match, I will use t.Errorf to print an error messagge
-    //and fail the test
-    for i := range actual {
-      word := actual[i]
-      expectedWord := c.expected[i]
-    }
-  }
+	actual := cleanInput(c.input)
+	// Check the length of the actual slice against the expected slice
+	// if they don't match, use t.Errorf to print an error message
+	// and fail the test
+	for i := range actual {
+		word := actual[i]
+		expectedWord := c.expected[i]
+		// Check each word in the slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+	}
+}
+
 }
